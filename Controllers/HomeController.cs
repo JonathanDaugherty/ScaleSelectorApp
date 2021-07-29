6d9e52d47116717efd6b843644eb47133eda7dac
jmdaugherty94@gmail.com <jmdaugherty94@gmail.com>
2021-07-29 16:40:24 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScaleSelectorApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+           public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View(); 
+        } 
+
+        public static string RandomKey(int textLength)
+        {
+            string[] keys = { "A", "A#", "Bb", "B", "C", "C#", "Db", "D", "D#", "Eb", "E", "F", "F#", "Gb", "G", "G#", "Ab" };
+            Random r = new Random();
+            int index = r.Next(keys.Length);
+
+            return keys[index];
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ScaleSelectorApp.MvcApplication" Language="C#" %>
